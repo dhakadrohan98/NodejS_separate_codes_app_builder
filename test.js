e210f1329ac288827a388c607d9729a54ba1d913
Rohan Dhakad <rohan.dhakad@sigmainfo.net>
2023-09-19 19:56:24 +0530

    update rma in magento with comment is completed

--- test.js
+++ test.js
@@ -6,3 +6,8 @@ console.log(obj);
 console.log("*********");
 obj.rmaDataObject.entity_id = "4"
 console.log(obj);
+
+var itemArray = [];
+itemArray.push({"key1":"val1"});
+itemArray.push({"key2":"val2"});
+console.log(itemArray);
